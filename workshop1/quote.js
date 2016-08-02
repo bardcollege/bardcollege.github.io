bf1a0c94cc0d5bc3fbf90db39d3b6e02516561b3
Keith O'Hara <kohara@bard.edu>
2016-08-02 18:50:30 -0400

    Revert "move quote into HTML"
    This reverts commit c0a630dcdb7dd13fa864f99272d809c0db5ead43.

---
+++ workshop1/quote.js
@@ -0,0 +1,46 @@
+/**
+ * Keith O'Hara <kohara@bard.edu>
+ * July 2016
+ * L&T quote generator
+ */
+
+var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
+var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
+var castles = ["museums", "bridges", "cities"]
+var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
+var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
+
+var verbs = ["is", "is not", "is like", "includes", "excludes", "is beyond",
+	     "extends", "encodes", "represents", "signifies", "consists of"];
+
+function choosei(lst){
+    var i = Math.floor(Math.random() * lst.length);
+    return i;
+}
+
+function choose(lst){
+    return lst[choosei(lst)];
+}
+
+var pick = choosei(things);
+var thing = things[pick];
+var programmer = programmers[pick];
+var castles = choose(castles);
+var exertion = choose(exertions);
+var air = choose(airs);
+
+var t = document.getElementById("thing").innerHTML = thing;
+var p = document.getElementById("person").innerHTML = programmer + "s";
+var q = document.getElementById("quote");
+
+q.innerHTML =
+    "\"The " + programmer + ", like the poet, works only slightly removed from pure thought-stuff. He builds his " + castles + " in the " + air + ", from " + air + ", creating by " + exertion + " of the imagination.\"" + "\u2014" +  "Fred Brooks, " + programmer;
+
+q.innerHTML += "<br><br><br>";
+
+for (var i = 0; i < verbs.length; i++){
+    q.innerHTML += "Code " + verbs[i] + " language. <br>";
+    q.innerHTML += "Language " + verbs[i] + " code. <br>";
+}
+
+q.innerHTML += "\u2014 For Loop";
